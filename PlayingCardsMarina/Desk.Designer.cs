bf6095b7f70f60c74cb674c6bb88bb22cbe81233
ma.rediko@gmail.com <ma.rediko@gmail.com>
2020-08-08 13:35:04 +0300

    ver_13:34

---
+++ PlayingCardsMarina/Desk.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace PlayingCardsMarina
+{
+    partial class Desk
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Desk));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.LoadCards = new System.Windows.Forms.ToolStripButton();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.LoadCards});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(800, 34);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // LoadCards
+            // 
+            this.LoadCards.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.LoadCards.Image = ((System.Drawing.Image)(resources.GetObject("LoadCards.Image")));
+            this.LoadCards.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.LoadCards.Name = "LoadCards";
+            this.LoadCards.Size = new System.Drawing.Size(105, 29);
+            this.LoadCards.Text = "Load Cards";
+            this.LoadCards.Click += new System.EventHandler(this.LoadCards_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton LoadCards;
+    }
+}
+
---
+++ PlayingCardsMarina/Desk.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace PlayingCardsMarina
+{
+    public partial class Desk : Form
+    {
+
+        private string folderPath = null;
+        private string [] fileNames = null;
+
+
+        public Desk()
+        {
+            InitializeComponent();
+            InitializeDesk();
+        }
+
+        private void InitializeDesk()
+        {
+            this.BackColor = Color.ForestGreen;
+          
+        }
+
+        private void LoadCards_Click(object sender, EventArgs e)
+        {
+            PictureBox filePictureBox = null;
+
+            folderPath = @"C:\Users\Installer\Desktop\Programming\Playing Cards\Playing Cards\playing_card_images\face";
+            fileNames = Directory.GetFiles(folderPath);
+
+            foreach(var fileName in fileNames)
+            {
+                filePictureBox = new PictureBox()
+                {
+                    Height = 100,
+                    Width = 70,
+                    SizeMode = PictureBoxSizeMode.StretchImage,
+                    Left = 100,
+                    Top = 100,
+                    Image = Image.FromFile(fileName)
+                };
+                this.Controls.Add(filePictureBox);
+            }
+        }
+    }
+}
